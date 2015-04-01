2ae2a4f438623b5f7edca5396790df8184f27fd7
riskihajar <riskihajar@gmail.com>
2015-04-01 20:58:38 +0800

    lib

---
+++ lib/lang/en/tanggal.php
@@ -0,0 +1,32 @@
+<?php
+return array (
+    'day' => array(
+        'Mon' => 'Monday',
+        'Tue' => 'Tuesday',
+        'Wed' => 'Wednesday',
+        'Thu' => 'Thursday',
+        'Fri' => 'Friday',
+        'Sat' => 'Saturday',
+        'Sun' => 'Sunday'
+    ),
+    'month' => array(
+        1  => 'January',
+        2  => 'February',
+        3  => 'March',
+        4  => 'April',
+        5  => 'May',
+        6  => 'June',
+        7  => 'July',
+        8  => 'August',
+        9  => 'September',
+        10 => 'October',
+        11 => 'November',
+        12 => 'December'
+    ),
+    'time' => array(
+        'dt-separator'     => 'at',
+        'minute-separator' => 'past',
+        'minute'           => 'minutes',
+        'second'           => 'seconds'
+    )
+);
---
+++ lib/lang/id/tanggal.php
@@ -0,0 +1,32 @@
+<?php
+return array (
+    'day' => array(
+        'Mon' => 'Senin',
+        'Tue' => 'Selasa',
+        'Wed' => 'Rabu',
+        'Thu' => 'Kamis',
+        'Fri' => 'Jum\'at',
+        'Sat' => 'Sabtu',
+        'Sun' => 'Minggu'
+    ),
+    'month' => array(
+        1  => 'Januari',
+        2  => 'Februari',
+        3  => 'Maret',
+        4  => 'April',
+        5  => 'Mei',
+        6  => 'Juni',
+        7  => 'Juli',
+        8  => 'Agustus',
+        9  => 'September',
+        10 => 'Oktober',
+        11 => 'Nopember',
+        12 => 'Desember'
+    ),
+    'time' => array(
+        'dt-separator'     => 'pukul',
+        'minute-separator' => 'lewat',
+        'minute'           => 'menit',
+        'second'           => 'detik'
+    )
+);
