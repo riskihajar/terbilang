bdbe88c4f22441da8d158a6554c9960f76aee88c
David Adi Nugroho <davidadi216@gmail.com>
2019-10-07 07:24:19 +0700

    fix incompatible test setUp

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,7 @@ abstract class TestCase extends BaseTestCase
 {
     protected $terbilang;
 
-    protected function setUp()
+    protected function setUp() : void
     {
         parent::setUp();
         $this->terbilang = new Terbilang();
