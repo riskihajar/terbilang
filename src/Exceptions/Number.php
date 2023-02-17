477a8b5e26f363070e633ba809ed95ac89a862cd
riskihajar <riskihajar@gmail.com>
2023-02-18 02:48:24 +0800

    wip

---
+++ src/Exceptions/Number.php
@@ -0,0 +1,17 @@
+<?php
+namespace Riskihajar\Terbilang\Exceptions;
+
+use Exception;
+
+class Number extends Exception
+{
+    public static function exceed(): self
+    {
+        return new static('NumToWords only accepts numbers between -' . PHP_INT_MIN . ' and ' . PHP_INT_MAX);
+    }
+
+    public static function notNumeric(): self
+    {
+        return new static('Number paramaters is not numeric');
+    }
+}
---
+++ src/NumberToWords.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Riskihajar\Terbilang;
+
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Stringable;
+use Illuminate\Support\Str;
+use Riskihajar\Terbilang\Exceptions\Number;
+
+class NumberToWords
+{
+    private $hyphen;
+    private $conjunction;
+    private $separator;
+    private $negative;
+    private $decimal;
+    private $dictionary;
+    private $prefix;
+    private $suffix;
+    private $prenum;
+    private $lang;
+
+    public function __construct()
+    {
+        $this->hyphen = Lang::get('terbilang::terbilang.hyphen');
+        $this->conjunction = Lang::get('terbilang::terbilang.conjunction');
+        $this->separator = Lang::get('terbilang::terbilang.separator');
+        $this->negative = Lang::get('terbilang::terbilang.negative');
+        $this->decimal = Lang::get('terbilang::terbilang.decimal');
+        $this->dictionary = Lang::get('terbilang::terbilang.dictionary');
+        $this->prefix = Lang::get('terbilang::terbilang.prefix');
+        $this->suffix = Lang::get('terbilang::terbilang.suffix');
+        $this->prenum = Lang::get('terbilang::terbilang.prenum');
+
+        $this->lang = Config::get('app.locale');
+    }
+
+    /**
+     * @param string|null $suffix
+     * @return NumberToWords
+     */
+    public function suffix(string|null $suffix): self
+    {
+        $this->suffix = $suffix;
+
+        return $this;
+    }
+
+    /**
+     * @param string|null $prefix
+     * @return NumberToWords
+     */
+    public function prefix(string|null $prefix): self
+    {
+        $this->prefix = $prefix;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $number
+     * @return mixed
+     * @throws Number
+     */
+    private function parseNumber(mixed $number): mixed
+    {
+        // parse quoted value and make sure its number
+        $number = doubleval($number);
+
+        // handle scientific value like 1.0E+15 after parse quoted
+        $number = sprintf('%0d', $number);
+
+        if ( ($number >= 0 && intval($number) < 0) || (intval($number) < 0 - PHP_INT_MAX)) {
+            throw Exceptions\Number::exceed();
+        }
+
+
+        return $number;
+    }
+
+    /**
+     * @param mixed $number
+     * @return Stringable
+     * @throws Number
+     */
+    public function make($number): Stringable
+    {
+        if(!is_numeric($number)){
+            throw Exceptions\Number::notNumeric();
+        }
+
+        $number = $this->parseNumber(number: $number);
+
+        if($number < 0) {
+            return $this->make(number: abs($number))->prepend($this->negative, ' ');
+        }
+
+        $string = $fraction = null;
+
+        if(strpos($number, '.') !== false) {
+            [$number, $fraction] = explode('.', $number);
+        }
+
+        switch (true) {
+            case $number < 21:
+                $string = $this->dictionary[$number];
+                break;
+            case $number < 100:
+                $tens   = ((int) ($number / 10)) * 10;
+                $units  = $number % 10;
+                $string = $this->dictionary[$tens];
+                if ($units) {
+                    $string .= $this->hyphen . $this->dictionary[$units];
+                }
+                break;
+            case $number < 1000:
+                $hundreds  = $number / 100;
+                $remains = $number % 100;
+                if ($this->prenum) {
+                    $lead = (int) substr($number, 0, 1);
+                    $string = ($lead === 1 ? $this->prenum : $this->dictionary[$hundreds] . ' ') . $this->dictionary[100];
+                } else {
+                    $string = $this->dictionary[$hundreds] . ' ' . $this->dictionary[100];
+                }
+                if ($remains) {
+                    $string .= $this->conjunction . $this->make($remains);
+                }
+                break;
+            default:
+                $baseUnit = pow(1000, floor(log($number, 1000)));
+                $numBaseUnits = (int) ($number / $baseUnit);
+                $remains = $number % $baseUnit;
+
+                if ($this->prenum) {
+                    $string = ($numBaseUnits === 1 && $baseUnit < 1000000 ? $this->prenum : $this->make($numBaseUnits) . ' ') . $this->dictionary[$baseUnit];
+                } else {
+                    $string = $this->make($numBaseUnits) . ' ' . $this->dictionary[$baseUnit];
+                }
+
+                if ($remains) {
+                    $string .= $remains < 100 ? $this->conjunction : $this->separator;
+                    $string .= $this->make($remains);
+                }
+                break;
+        }
+
+        if (null !== $fraction && is_numeric($fraction)) {
+            $string .= $this->decimal;
+            $words = array();
+            foreach (str_split((string) $fraction) as $number) {
+                $words[] = $this->dictionary[$number];
+            }
+            $string .= implode(' ', $words);
+        }
+
+        return Str::of($string)
+            ->prepend($this->prefix, $this->prefix ? ' ' : '')
+            ->append($this->suffix, $this->suffix ? ' ' : '');
+    }
+
+}
